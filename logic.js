754c9624008ca3996e1a3efb65b8bd2cd42d90dd
joooons <joon.s.chang@gmail.com>
2020-09-13 16:38:03 -0700

    able to convert Eng to Heb html entities

--- logic.js
+++ logic.js
@@ -90,10 +90,201 @@ const editBox = {};
         return arr;
     }
 
+    
+
+const En_to_He = {
+    'a' : '&#1488;',
+    'A' : '&#1470;',
+
+    'b' : '&#1489;',
+    'B' : '&#64305;',
+    
+    'c' : '&#64299;',
+    'C' : '&#64301;',
+
+    'd' : '&#1491;',
+    'D' : '&#64307;',
+
+    'e' : '&#1461;',
+    'E' : '&#1461;',
+
+    'f' : '&#1496;',
+    'F' : '&#64312;',
+
+    'g' : '&#1490;',
+    'G' : '&#64306;',
+
+    'h' : '&#1492;',
+    'H' : '&#64308;',
+
+    'i' : '&#1460;',
+    'I' : '&#1460;',
+
+    'j' : '&#1495;',
+    'J' : '&#1498;&#1456;',
+
+    'k' : '&#1499;',
+    'K' : '&#64315;',
+
+    'l' : '&#1500;',
+    'L' : '&#64316;',
+
+    'm' : '&#1502;',
+    'M' : '&#64318;',
+
+    'n' : '&#1504;',
+    'N' : '&#64320;',
+    
+    'o' : '&#1465;',
+    'O' : '&#1465;',
+    'ø' : '&#1465;',
+
+    'p' : '&#1508;',
+    'P' : '&#64324;',
+
+    'q' : '&#1511;',
+    'Q' : '&#64327;',
+
+    'r' : '&#1512;',
+
+    't' : '&#1514;',
+    'T' : '&#64330;',
+
+    's' : '&#1505;',
+    'S' : '&#64321;',
+
+    'u' : '&#1467;',
+
+    'v' : '&#64298;',
+    'V' : '&#64300;',
+
+    'w' : '&#1493;',
+    'W' : '&#64309;',
+
+    'x' : '&#1510;',
+    'X' : '&#64326;',
+
+    'y' : '&#1497;',
+    'Y' : '&#64313;',
+    // 'y' : 'י',
+
+    'z' : '&#1494;',
+    'Z' : '&#64310;',
+
+
+    '°' : '&#1498;',
+    '$' : '&#1498;',
+
+    'π' : '&#1507;',
+
+    'μ' : '&#1501;',
+
+    '#' : '&#1509;',
+    "≈" : '&#1509;',
+
+    '!' : '&#1503;',
+    '˜' : '&#1503;',
+    
+    '&' : '&#1451;',
+    '‡' : '&#1451;',
+    ',' : '&#1462;', 
+    "\<" : '&#1462;',
+
+    "\–" : '&#1425;',
+
+    ';' : '&#1464;',
+    "\:" : '&#1464;',
+
+    '∂' : '&#1468;',
+    '{' : '&nbsp;',
+    '}' : '&#1458;',    
+    
+    "\‘" : '&#1457;',
+
+    "\'" : '&#1463;',
+    "\"" : '&#1463;',
+
+    "\[" : '&#1506;',
+
+    "\]" : '&#1456;',
+    "\“" : '&#1456;',
+
+    "\/" : '&#64331;',
+
+    "÷" : '/'
+    
+};
+
+const vowels = [
+    'e', 'E', 'i', 'I', 'o', 'O', 'ø', 'u', '&', ',', '\<',
+    '\–', ';', '\:', '}', '\‘', "\'", '\"', '\]', '\“', '‡'
+]
+
+
+$('#experiment').on('change', function() {
+    let str = $('#experiment').val();
+
+    let arr = strToSyllableArr(str).reverse();
+    str = arr.join('');
+    arr = str.split('');
+
+    Object.keys(En_to_He).forEach( key => {
+        arr.forEach( (char,i) => {
+            if ( char == key ) arr[i] = En_to_He[key];
+        });
+    });
+    
+    str = arr.join('');
+
+    $('#output').html( str );
+    $('#experiment').val('');
+});
+
+// function reverseStr(str) { return str.split('').reverse().join(''); }
+    // Not used at the moment
+
+function strToSyllableArr(str) {
+    let arr = str.split('');
+    let newArr = [];
+    arr.forEach( (char, i) => {
+        if ( !vowels.includes(char) ) {
+            let temp = char;
+            doThis(i+1);
+            newArr.push(temp);
+            function doThis(num) {
+                if ( num >= arr.length ) return;
+                if ( vowels.includes(arr[num]) ) {
+                    temp += arr[num];
+                    doThis(num+1);
+                }
+            }
+        }
+    });
+    console.log(newArr);
+    ScootTheDagh(newArr);
+    function ScootTheDagh(arr) {
+        if ( arr[0] == '∂' ) {
+            arr.push(arr.shift());
+            arr.push(arr.shift());
+        }
+    }
+    return newArr;
+}
 
 
 
 
+
+function showHE(a,b) {
+    let str = '';
+    if (!b) { str += `${a} &nbsp;&nbsp;&nbsp;&#${a};&#10;`; } 
+    else if ( a < b ) {
+        for ( i=a ; i<=b ; i++ ) { str += `${i} &nbsp;&nbsp;&nbsp;&#${i};&#10;`; }
+    }
+    $('#total').html(str);
+    // showHE(1488, 1522);
+    // showHE(64285, 64334);
+}
 
 
 
