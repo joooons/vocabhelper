619cd622bd9bdd98e916328c4d2c8a54ba8c137a
joooons <52220710+joooons@users.noreply.github.com>
2024-03-01 11:52:27 -0800

    test to see if index.js is used

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const app = require('express')();
-const http = require('http')
-    .createServer(app)
-    .listen(3000);
-const io = require('socket.io')(http);
+// const app = require('express')();
+// const http = require('http')
+//     .createServer(app)
+//     .listen(3000);
+// const io = require('socket.io')(http);
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
