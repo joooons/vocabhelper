55cd4ff953bf05bc7af262cac5280e10603fcf74
joooons <52220710+joooons@users.noreply.github.com>
2024-03-01 11:55:54 -0800

    mod gitignore; remove index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// const app = require('express')();
-// const http = require('http')
-//     .createServer(app)
-//     .listen(3000);
-// const io = require('socket.io')(http);
-
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
